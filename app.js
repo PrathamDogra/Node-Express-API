f997ff56734b799befdf8ce6011eb50eb68f49bb
PrathamDogra <dogra.pratham@gmail.com>
2019-07-12 12:03:44 +0530

    created a route called products

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express')
 
+
 // the app executes the express like a function
 const app = express();
+
+const productRoutes = require('./api/routes/products');
+
 //Below is a middleware
-app.use((req, res, next) =>{
-    res.status(200).json({
-        message:'The code works',
-    })
-})
+// app.use((req, res, next) =>{
+//     res.status(200).json({
+//         message:'The code works',
+//     })
+// })
+
+app.use('/products', productRoutes);
 
 module.exports = app;
