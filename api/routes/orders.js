b79ff229bcd810a03c10d065b0e91fe756efae31
PrathamDogra <dogra.pratham@gmail.com>
2019-07-16 15:33:20 +0530

    body Parser

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -10,9 +10,14 @@ router.get('/', (req,res,next)=>{
 })
 
 router.post('/',(req,res,next)=>{
+    const order={
+        productId:req.body.productId,
+        quantity:req.body.quantity
+    }
     res.status(200).json(
         {
-            message:'Orders are being created'
+            message:'Orders are being created',
+            order:order
         }
     )
 })
--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const app = express();
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders')
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+//For logging the error
 app.use(morgan('dev'));
-
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 // /products is a filter, only requests that begins with /products can go to the middleware productRoutes.
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
